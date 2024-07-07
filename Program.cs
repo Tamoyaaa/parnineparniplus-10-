8b60f0d02dc287fb941c5e0cc0ff9af7416204eb
Tamoyaaaaa <7amoyka@gmail.com>
2024-07-07 16:59:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace parnineparniplus_10_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число:");
+            int n = int.Parse(Console.ReadLine());
+
+            int koliche = 0;
+            int sum = 0;
+            int i = 1;
+
+            while (i <= n)
+            {
+                if (i % 2 != 0)
+                {
+                    koliche++;
+                    sum += i;
+                }
+                i++;
+            }
+            Console.WriteLine($"Количетсво нечетных чисел:{koliche}");
+            Console.WriteLine($"Сумма нечетных чисел:{sum}");
+        }
+    }
+}
